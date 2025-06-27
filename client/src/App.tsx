74572c149b91f38ebc17b2e5fed76857dd8b2297
MogesBekele <mogesbekele32@gmail.com>
2025-06-27 17:41:55 +0300

    create the form in hero

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { useState } from "react";
-import { Route, Routes,  } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CarDetails from "./pages/CarDetails";
 import Cars from "./pages/Cars";
@@ -11,20 +11,23 @@ const App = () => {
   const [showLogin, setShowLogin] = useState<boolean>(false);
   const isOwnerPath = useLocation().pathname.startsWith("/owner");
 
-  return(
-   <>
-  {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
-
-  <Routes>
-    <Route path="/" element={<Home/>} />
-    <Route path="/car-details/:id" element={<CarDetails/>} />
-    <Route path="/cars" element={<Cars/>} />
-    <Route path="/my-bookings" element={<MyBookings/>} />
-    {/* Add more routes as needed */}
-
-  </Routes>
-  </>
-  )
+  return (
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+      <div
+        className="pt-24 bg-light
+"
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/car-details/:id" element={<CarDetails />} />
+          <Route path="/cars" element={<Cars />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </>
+  );
 };
 
 export default App;
