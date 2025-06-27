8cb4b01d6b666263af4b8009a5dde19a759ef536
MogesBekele <mogesbekele32@gmail.com>
2025-06-27 15:59:38 +0300

    comlete the navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -8,10 +8,14 @@ type MenuLink = {
   path: string;
 };
 
-const Navbar = ({ setShowLogin }: { setShowLogin: (show: boolean) => void }) => {
+const Navbar = ({
+  setShowLogin,
+}: {
+  setShowLogin: (show: boolean) => void;
+}) => {
   const location = useLocation();
   const [open, setOpen] = useState<boolean>(false);
-  const navigate= useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div
@@ -25,7 +29,8 @@ const Navbar = ({ setShowLogin }: { setShowLogin: (show: boolean) => void }) => 
       <div
         className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${
           location.pathname === "/" ? "bg-light" : "bg-white"
-        }  ${open ? "max-sm:translate-x-0" : "max-sm:-translate-x-full"}`}
+        }  ${open ? "max-sm:translate-x-0" : "max-sm:-translate-x-full text-center"}`}
+        
       >
         {menuLinks.map((link: MenuLink, index: number) => (
           <Link key={index} to={link.path}>
@@ -33,14 +38,28 @@ const Navbar = ({ setShowLogin }: { setShowLogin: (show: boolean) => void }) => 
           </Link>
         ))}
         <div className="hidden lg:flex items-center gap-2 border border-borderColor px-3 rounded-full max-w-56 ">
-          <input type="text" placeholder="search products" className="py-1.5 w-full bg-transparent outline-none placeholder:gray-500 " />
-          <img src={assets.search_icon} alt="search"  />
+          <input
+            type="text"
+            placeholder="search products"
+            className="py-1.5 w-full bg-transparent outline-none placeholder:gray-500 "
+          />
+          <img src={assets.search_icon} alt="search" />
         </div>
         <div className="flex max-sm:flex-col items-start sm:items-center gap-6">
-          <button onClick={()=>navigate('/owner')} className="cursor-pointer">Dashboard</button>
-          <button onClick={()=>setShowLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg ">Login</button>
+          <button onClick={() => navigate("/owner")} className="cursor-pointer">
+            Dashboard
+          </button>
+          <button
+            onClick={() => setShowLogin(true)}
+            className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg "
+          >
+            Login
+          </button>
         </div>
       </div>
+      <button className="sm:hidden cursor-pointer" onClick={() => setOpen(!open)}>
+        <img src={open ? assets.close_icon : assets.menu_icon} alt="menu" />
+      </button>
     </div>
   );
 };
