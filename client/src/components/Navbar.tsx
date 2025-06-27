256216aa7451ea352107442d7ef7b05845966556
MogesBekele <mogesbekele32@gmail.com>
2025-06-27 12:35:09 +0300

    js to ts in nav

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,30 +1,39 @@
-import React, { useState } from 'react'
-import {assets, menuLinks} from '../assets/assets'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import { assets, menuLinks } from "../assets/assets";
+import { Link, useLocation } from "react-router-dom";
 
+// Define the type for menuLinks if not already typed
+type MenuLink = {
+  name: string;
+  path: string;
+};
 
 const Navbar = () => {
-  const location = useLocation()
-  const [open, setOpen] =useState <boolean> (false)
+  const location = useLocation();
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all  ${location.pathname === '/' && 'bg-light'}`} >
-      <Link to='/'>
-            <img src={assets.logo} alt="logo"  className='h-8' />
+    <div
+      className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all  ${
+        location.pathname === "/" && "bg-light"
+      }`}
+    >
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="h-8" />
       </Link>
-      <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === '/' ? 'bg-light' : 'bg-white'}  ${open? 'max-sm:translate-x-0': 'max-sm:-translate-x-full'}`}>
-        {
-        menuLinks.map((link:any, index:any)=>(
+      <div
+        className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${
+          location.pathname === "/" ? "bg-light" : "bg-white"
+        }  ${open ? "max-sm:translate-x-0" : "max-sm:-translate-x-full"}`}
+      >
+        {menuLinks.map((link: MenuLink, index: number) => (
           <Link key={index} to={link.path}>
-          {link.name}
+            {link.name}
           </Link>
-        ))
-        }
+        ))}
       </div>
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
