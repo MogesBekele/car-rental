1a07acac0bba2b84848969f2ff7c7a27d412ac85
MogesBekele <mogesbekele32@gmail.com>
2025-06-27 16:34:10 +0300

    mybookings

--- client/src/pages/Cars.tsx
+++ client/src/pages/Cars.tsx
@@ -7,7 +7,7 @@ const Cars = () => {
       <h1>Available Cars</h1>
       <p>Explore our collection of cars available for rent.</p>
       {/* Add more content or components as needed */}
-      <Link to={"/car-details/:id"}>car details</Link>
+      <Link to={"/car-details/:id"}>car detail</Link>
       
     </div>
   )
--- client/src/pages/MyBookings.tsx
+++ client/src/pages/MyBookings.tsx
@@ -3,6 +3,9 @@
 const MyBookings = () => {
   return (
     <div>
+      <h1>My Bookings</h1>
+      <p>View and manage your bookings here.</p>
+      {/* Add more content or components as needed */}
       
     </div>
   )
