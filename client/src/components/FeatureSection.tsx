7dfe34f4fbf167be8f8e21016bf9280f0d6db27d
MogesBekele <mogesbekele32@gmail.com>
2025-06-29 12:24:25 +0300

    create feature and title components

---
+++ client/src/components/FeatureSection.tsx
@@ -0,0 +1,15 @@
+import Title from "./Title"
+
+
+const FeatureSection = () => {
+  return (
+    <div className="flex flex-col items-center py-24 px-6 md:px-16 lg:px-24 xl:px-32">
+      <div>
+        <Title title="Features Vehicles" subTitle="Explore our selection of premium vehicles available for your next adventure" align="center"/>
+      </div>
+      
+    </div>
+  )
+}
+
+export default FeatureSection
