54cbbf4686e094fe733973ef0d8eb913840cc911
MogesBekele <mogesbekele32@gmail.com>
2025-07-02 11:00:55 +0300

    my Bookings page now uses useState to manage bookings state

--- client/src/pages/MyBookings.tsx
+++ client/src/pages/MyBookings.tsx
@@ -1,11 +1,20 @@
-
+import { useEffect, useState } from "react"
+import { dummyMyBookingsData } from "../assets/assets"
+import Title from "../components/Title"
 
 const MyBookings = () => {
+  const [bookings, setBookings] = useState<any>([])
+
+  const fetchBookings = async ()=>{
+    setBookings(dummyMyBookingsData)
+  }
+  useEffect(()=>{
+    fetchBookings()
+
+  },[])
   return (
-    <div>
-      <h1>My Bookings</h1>
-      <p>View and manage your bookings here.</p>
-      {/* Add more content or components as needed */}
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32  2xl:px-48 mt-16 text-sm max-w-7xl">
+      <Title title="My Bookings" subTitle="View and manage your bookings here" align="left"/>
       
     </div>
   )
