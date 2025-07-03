1b336af0775b60bea02e9a8d077f575c13d1a11e
MogesBekele <mogesbekele32@gmail.com>
2025-07-03 11:13:11 +0300

    create sidebar component

--- client/src/pages/owner/Layout.tsx
+++ client/src/pages/owner/Layout.tsx
@@ -1,15 +1,17 @@
-import AddCar from "./AddCar"
-
-
+import { Outlet } from "react-router-dom";
+import NavbarOwner from "../../components/owner/NavbarOwner";
+import Sidebar from "../../components/owner/Sidebar";
 
 const Layout = () => {
   return (
-    <div>
-      <AddCar/>
-    
-      
+    <div className="flex flex-col">
+      <NavbarOwner />
+      <div className="flex">
+        <Sidebar />
+        <Outlet />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
