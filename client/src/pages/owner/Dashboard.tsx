38b29f4017053527cd32d8d0fdee55a56ca312f2
MogesBekele <mogesbekele32@gmail.com>
2025-07-04 11:28:48 +0300

    complete dashboard

--- client/src/pages/owner/Dashboard.tsx
+++ client/src/pages/owner/Dashboard.tsx
@@ -15,8 +15,9 @@ type Card = {
   title: string;
   value: number;
   icon: string;
-}
+};
 const Dashboard = () => {
+  const currency = import.meta.env.VITE_CURRENCY;
   // 2. Type your state
   const [data, setData] = useState<DashboardData>({
     totalCars: 0,
@@ -48,7 +49,7 @@ const Dashboard = () => {
       value: data.completedBookings,
       icon: assets.listIconColored,
     },
-  ]
+  ];
 
   useEffect(() => {
     setData(dummyDashboardData);
@@ -56,20 +57,63 @@ const Dashboard = () => {
 
   return (
     <div className="px-4 pt-10 md:px-10 flex-1">
-      <Title title=" AdminDashboard" subTitle="Monitor overall platform performance including total cars, bookings, revenue and recent activities "/>
+      <Title
+        title=" AdminDashboard"
+        subTitle="Monitor overall platform performance including total cars, bookings, revenue and recent activities "
+      />
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl">
-        {
-          dashboardCards.map((card, index)=>(
-            <div key={index} className=" flex gap-2 items-center justify-between p-4 rounded-md border border-borderColor">
-              <div>
-                <h1 className="text-xs text-gray-500">{card.title}</h1>
-                <p className="text-lg font-semibold">{card.value}</p>
-              </div>
-
+        {dashboardCards.map((card, index) => (
+          <div
+            key={index}
+            className=" flex gap-2 items-center justify-between p-4 rounded-md border border-borderColor"
+          >
+            <div>
+              <h1 className="text-xs text-gray-500">{card.title}</h1>
+              <p className="text-lg font-semibold">{card.value}</p>
             </div>
-          ))
-        }
-
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-primary/10">
+              <img src={card.icon} alt="" className="w-4 h-4" />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-wrap items-start gap-6 mb-8 w-full">
+        {/* {recent bookings} */}
+        <div className="p-4 md:p-6 border border-borderColor rounded-md max-w-lg w-full">
+          <h1 className="text-lg font-medium">Recent Bookings</h1>
+          <p className="text-gray-500">Latest customer bookings</p>
+          {data.recentBookings.map((booking, index) => (
+            <div key={index} className="flex items-center mt-4 justify-between">
+              <div className="flex items-center gap-2">
+                <div className="hidden md:flex items-center justify-center w-12 h-12 rounded-full bg-primary/10">
+                  <img
+                    src={assets.listIconColored}
+                    className="h-5 w-5"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <p>
+                    {booking.car.brand} {booking.car.model}
+                  </p>
+                  <p className="tex-sm text-gray-500">
+                    {booking.createdAt.split("T")[0]}
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center gap-2 font-medium">
+                <p className="text-sm text-gray-500">{currency}{booking.price}</p>
+                <p className="px-3 py-0.5 border border-borderColor rounded-full text-sm">{booking.status}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* {monthly revenue} */}
+        <div className="p-4 md:p-6 mb-6 border border-borderColor rounded-md w-full md:max-w-xs">
+          <h1 className="text-lg font-medium">Monthly Revenue</h1>
+          <p className="text-gray-500">Revenue for current month</p>
+          <p className="text-3xl mt-6 font-semibold text-primary">{currency}{data.monthlyRevenue}</p>
+        </div>
       </div>
     </div>
   );
