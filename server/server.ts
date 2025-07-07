7e76a181c4819df9b79b46dc4df1d02aa415a0f7
MogesBekele <mogesbekele32@gmail.com>
2025-07-07 10:17:35 +0300

    CREATE EXPRESS APP

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
+import 'dotenv/config'
 
 const app = express();
-
+//middleware
 app.use(cors());
 app.use(express.json());
 
-app.listen(3000, () => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
