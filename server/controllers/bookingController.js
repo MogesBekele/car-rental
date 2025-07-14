0fa219f3722a131ad8c53fed89adf063eee9a1b0
MogesBekele <mogesbekele32@gmail.com>
2025-07-14 10:49:38 +0300

    filter with location and pichupdate

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,36 +1,39 @@
-import Booking from "../models/bookingModel"
-import Car from "../models/carModel"
+import Booking from "../models/bookingModel";
+import Car from "../models/carModel";
 
- const checkAvailability = async (car, pickupDate, returnDate)=>{
+const checkAvailability = async (car, pickupDate, returnDate) => {
   const bookings = await Booking.find({
     car,
     pickupDate: { $lte: pickupDate },
     returnDate: { $gte: returnDate },
-  })
+  });
   return bookings.length === 0;
-
-}
+};
 
 //API to check Availability of cars for the given date and loncation
 
-export const chechAvialabilityOfCars = async (req, res)=>{
+export const chechAvialabilityOfCars = async (req, res) => {
   try {
-    const {location, pickupDte, returnDate}= req.body;
-    //fetch all available cars for the given location 
+    const { location, pickupDte, returnDate } = req.body;
+    //fetch all available cars for the given location
 
-    const cars= await Car.find({location, isAvailable: true})
+    const cars = await Car.find({ location, isAvailable: true });
     //check availablity for the given date range using promise
 
-    const availableCarsPromises = cars.map(async(car)=>{
-      const isAvailable = await checkAvailability(car._id, pickupDte, returnDate)
-      return {...car._id, isAvailable: isAvailable}
-    })
-    
+    const availableCarsPromises = cars.map(async (car) => {
+      const isAvailable = await checkAvailability(
+        car._id,
+        pickupDte,
+        returnDate
+      );
+      return { ...car._id, isAvailable: isAvailable };
+    });
+
     let availableCars = await Promise.all(availableCarsPromises);
-    availableCars = availableCars.filter((car=>car.isAvailable === true))
-    res.json({success: true, availableCars})
+    availableCars = availableCars.filter((car) => car.isAvailable === true);
+    res.json({ success: true, availableCars });
   } catch (error) {
-    console.log(error)
-    res.json({success: false, message: error.message})
+    console.log(error);
+    res.json({ success: false, message: error.message });
   }
-}
+};
