fd85e1392feced2209cfb433d81adf64ba80022f
MogesBekele <mogesbekele32@gmail.com>
2025-07-14 10:47:00 +0300

    check cars availablity

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -12,4 +12,24 @@ import Booking from "../models/bookingModel"
 
 //API to check Availability of cars for the given date and loncation
 
-export const chechAvialabilityOfCars = async
+export const chechAvialabilityOfCars = async (req, res)=>{
+  try {
+    const {location, pickupDte, returnDate}= req.body;
+    //fetch all available cars for the given location 
+
+    const cars= await Car.find({location, isAvailable: true})
+    //check availablity for the given date range using promise
+
+    const availableCarsPromises = cars.map(async(car)=>{
+      const isAvailable = await checkAvailability(car._id, pickupDte, returnDate)
+      return {...car._id, isAvailable: isAvailable}
+    })
+    
+    let availableCars = await Promise.all(availableCarsPromises);
+    availableCars = availableCars.filter((car=>car.isAvailable === true))
+    res.json({success: true, availableCars})
+  } catch (error) {
+    console.log(error)
+    res.json({success: false, message: error.message})
+  }
+}
