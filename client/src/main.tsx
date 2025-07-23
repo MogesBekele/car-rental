5136736f2c0ac2b348c8df2990cc25b2246ffe59
MogesBekele <mogesbekele32@gmail.com>
2025-07-23 11:56:18 +0300

    fix the deployment error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { AppProvider } from "./context/AppContext.tsx";
+import { AppProvider } from "./context/AppContext";
 import { MotionConfig } from "motion/react";
 
 createRoot(document.getElementById("root")!).render(
